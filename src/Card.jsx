2b3f32af67f7ea8dd9a447f682eb26d1c8d295bf
Dharaneshwaran-R <dharaneshr29@gmail.com>
2025-10-16 10:25:19 +0530

    changed desc

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,7 @@ function Card(){
         <div className="card">
             <img className="profileimg" src={profile} alt="Profile"/>
             <h2 className="card-h2">Dharan</h2>
-            <p className="para">Work hard and Be Consitent</p>
+            <p className="para">Aspire Higher</p>
         </div>
     )
 }
